558659d7902893420e14a2e0b71ea49c3e66586e
mrf3 <mrf345@gmail.com>
2018-06-23 21:58:18 +0800

    fix top space

--- js/allin.js
+++ js/allin.js
@@ -145,7 +145,7 @@ jQuery(document).ready(function ($) {
           }
         }, function () {
           allIn.toCallForAll = function () {
-            //$('#thev').addClass('fixed-top')
+            $('#thev').addClass('fixed-top')
           }
           msgNotifier({
             url_hash: ['thanke', 'thankj'],
